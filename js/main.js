605d7ed4832dc7354e952dd811ed9a2ead6632c4
icetest <i_have_won1@ukr.net>
2013-07-31 10:09:57 +0300

    31-07

--- js/main.js
+++ js/main.js
@@ -79,12 +79,7 @@ $(document).ready(function() {
 
 
 	// --------------- Подключение jQuery Selectbox --------------- 
-	$('.custom-select').selectbox({
-		onChange: function(val) {
-			var id = $(this).attr('sb');
-			$('#sbSelector_' + id)[0].className = 'sbSelector ' + val;
-		}
-	});
+	$('.custom-select').selectbox({});
 
 
 });
